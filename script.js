7cf1db6ffbfe828cf5d4cb0d501216a0a20b32d9
sathish <2000sathishkumar.d@gmail.com>
2022-12-22 16:06:23 +0530

    add script

--- script.js
+++ script.js
@@ -5,4 +5,11 @@ const inputEl = document.getElementById("input");
 const butSubmitEl = document.getElementById("btn");
 const outputEl = document.getElementById("list-item");
 
-// functions
+// addek
+butSubmitEl.addEventListener("click", function () {
+  const todo = inputEl.value;
+  const list = document.createElement("li");
+
+  list.innerHTML = `${todo}<button >delete</button>`;
+  outputEl.appendChild(list);
+});
