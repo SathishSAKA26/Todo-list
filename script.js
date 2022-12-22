9abcbb433038e32dbe0e967ec70158d2d7e24cb8
sathish <2000sathishkumar.d@gmail.com>
2022-12-22 12:13:32 +0530

    add the codes

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strcat";
+
+// get elements
+const inputEl = document.getElementById("input");
+const butSubmitEl = document.getElementById("btn");
+const outputEl = document.getElementById("list-item");
+
+// functions
