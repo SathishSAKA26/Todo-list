f76636706f5a657d558b6ee1fc3cfda2ad850caa
sathish <2000sathishkumar.d@gmail.com>
2022-12-24 12:19:53 +0530

    compilit project

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 "use strcat";
 
 // get elements
-const inputEl = document.getElementById("input");
-const butSubmitEl = document.getElementById("btn");
+const titleEl = document.getElementById("input");
+const quantityEl = document.getElementById("quantity");
+const formEl = document.getElementById("form-submit");
 const outputEl = document.getElementById("list-item");
 
-// addek
-butSubmitEl.addEventListener("click", function () {
-  const todo = inputEl.value;
+// addEventListener
+formEl.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const listName = titleEl.value;
+  const quantity = quantityEl.value;
+
   const list = document.createElement("li");
 
-  list.innerHTML = `${todo}<button >delete</button>`;
+  list.innerHTML = `${listName} - ${quantity} <button>delete</button> <button>update</button>`;
   outputEl.appendChild(list);
 });
