070bcd13ca013ce70ee077ff562963e5df01f802
sathish <2000sathishkumar.d@gmail.com>
2022-12-28 15:55:14 +0530

    add  js

--- script.js
+++ script.js
@@ -2,27 +2,43 @@
 
 // get elements
 const titleEl = document.getElementById("input");
-// const quantityEl = document.getElementById("quantity");
 const formEl = document.getElementById("form-submit");
-const outputEl = document.getElementById("list-item");
+const containerEl = document.getElementById('list-item')
+// const quantityEl = document.getElementById("quantity");
+// const outputEl = document.getElementById("list-item");
 
-// addEventListener
-formEl.addEventListener("submit", function (event) {
-  event.preventDefault();
-  const listName = titleEl.value;
-  // const quantity = quantityEl.value;
+// initial element
+// let item = [
+//   {title: 'food'}
+//   {title: 'apple'}
+// ];
 
-  const list = document.createElement("li");
+const task = [
+  {title: 'apple'},
+  {title: 'banana'},
+  {title: 'oranges'}
+];
 
-  list.innerHTML = `${listName} 
-  <button><i class="fa-solid fa-pen-to-square"></i>
-  </button>
-  <button><i class="fa-solid fa-trash"></i></i>
-  </button>`;
 
-  // - ${quantity}
-  // <button><i class="fa-sharp fa-solid fa-upload"><i><button>
-  // <button><i class="fa-sharp fa-solid fa-trash-xmark"><i></button> `
 
-  outputEl.appendChild(list);
-});
+// // addEventListener
+
+// formEl.addEventListener("submit", function (e) {
+//   e.preventDefault();
+//   if()
+//   // const quantity = quantityEl.value;
+
+//   const list = document.createElement("li");
+
+//   list.innerHTML = `${listName}
+//   <button class='btn-edit' onclick='editItem'><i class="fa-solid fa-pen-to-square"></i>
+//   </button>
+//   <button class='btn-delete' onclick='deleteItem'><i class="fa-solid fa-trash"></i></i>
+//   </button>`;
+
+//   // - ${quantity}
+//   // <button><i class="fa-sharp fa-solid fa-upload"><i><button>
+//   // <button><i class="fa-sharp fa-solid fa-trash-xmark"><i></button> `
+
+//   outputEl.appendChild(list);
+// });
